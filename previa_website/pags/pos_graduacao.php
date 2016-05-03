a5b6dddbeb557f49dcc7de08b89547ce5f9fb900
eliasfank <eliasfank@hotmail.com>
2016-05-03 17:25:05 -0300

    página de pos graduação ajustada(dropdown retirado)

---
+++ previa_website/pags/pos_graduacao.php
@@ -0,0 +1,359 @@
+
+<div class="container" >
+
+	<div class="col-xs-2" style="border-radius: 15px; position:fixed; border: 1px solid; margin-top: 25px;">
+		<ol>
+			<a href="#um"><li>Apresentação Strictu Sensu</li></a>
+			<a href="#dois"><li>Apresentação Lato Sensu</li></a>
+			<a href="#tres"><li>Disciplinas</li></a>
+			<a href="#quatro"><li>Orientadores</li></a>
+			<a href="#cinco"><li>Editais</li></a>
+			<a href="#seis"><li>Bolsas</li></a>
+			<a href="#sete"><li>Horários</li></a>
+		</ol>
+	</div>
+
+	<div class="col-xs-9" style="float: right;">
+		<hr id="um">
+		<h2>Apresentação Strictu Sensu</h2>
+		<br>
+
+		A pós-graduação stricto sensu compreende os cursos de mestrado e doutorado acadêmicos e os cursos de mestrado profissional (Portaria Capes 80 de 1998). Chama-se Programa de Pós-Graduação ao conjunto de cursos de uma determinada área do conhecimento, consistindo usualmente do par mestrado-doutorado. Contudo, um programa pode também consistir de apenas um mestrado ou de mais de um curso de mestrado e/ou doutorado, desde que na mesma área. A UFF tem, em dezembro de 2009, 50 programas de pós-graduação aprovados pela CAPES; 31 destes programas consistem de mestrado e doutorado, 5 correspondem a mestrados profissionais e os 14 restantes, a mestrados acadêmicos. <p><a href="http://uffs.edu.br" style="background-color:#ffffa0">Clique aqui</a></p> para saber a relação dos programas, telefones, endereços e conceitos CAPES.
+
+		Somente possuem validade nacional os diplomas emitidos por instituições de ensino superior brasileiras, que provenham de programas de pós-graduação stricto sensu avaliados pela Capes, com nota igual ou superior a 3. Essa avaliação se dá a cada três anos, e leva em conta indicadores tais como a abrangência, áreas de concentração, metodologias de ensino, infra-estrutura utilizada, corpo docente e produção científica do programa, dentre outros. A oferta de cursos fora da sede, ou em quaisquer modalidades não previstas no projeto inicial avaliado e reconhecido pela Capes deve, necessariamente, ser submetida a nova avaliação. Para saber como proceder para criar um curso de pós-graduação na UFF, <p><a href="http://uffs.edu.br" style="background-color:#ffffa0">clique aqui.</a></p>
+		Texto: http://www.proppi.uff.br/posgraduacao/apresentacao-stricto-sensu.
+
+
+		<hr id="dois">
+		<h2>Apresentação Lato Sensu</h2>
+		<br>
+
+		A pós-graduação lato sensu - cursos de especialização / MBA / Residência - caracteriza-se pela formação de pessoal em nível de especialização profissional, visando à capacitação e ao aprimoramento do educando em áreas específicas do saber. É um programa de estudo de curta duração (mínimo de 360 horas) e com flexibilidade curricular em termos de conteúdo, disciplinas e atividades acadêmicas. Para a conclusão do curso, exige-se apresentação de trabalho final individual ou monografia. É aberta a candidatos diplomados em cursos superiores e que atendam às exigências das instituições de ensino - art. 44, III, Lei 9.394/1996. <p><a href="http://uffs.edu.br" style="background-color:#ffffa0">clique aqui.</a></p>
+		Texto:http://www.proppi.uff.br/posgraduacao/apresentacao-lato-sensu.
+		<br><br>
+
+		A pós-graduação lato sensu - cursos de especialização / MBA / Residência - caracteriza-se pela formação de pessoal em nível de especialização profissional, visando à capacitação e ao aprimoramento do educando em áreas específicas do saber. É um programa de estudo de curta duração (mínimo de 360 horas) e com flexibilidade curricular em termos de conteúdo, disciplinas e atividades acadêmicas. Para a conclusão do curso, exige-se apresentação de trabalho final individual ou monografia. É aberta a candidatos diplomados em cursos superiores e que atendam às exigências das instituições de ensino - art. 44, III, Lei 9.394/1996. <p><a href="http://uffs.edu.br" style="background-color:#ffffa0">clique aqui.</a></p>
+		Texto:http://www.proppi.uff.br/posgraduacao/apresentacao-lato-sensu.
+
+
+		<hr id="tres">
+		<h2>Disciplinas</h2>
+		<br>
+		<table class="table table-striped">
+		<thead> 
+			<tr> 
+			<th>Área</th> 
+			<th>Disciplina</th> 
+			<th>Tipo</th> 
+			</tr> 
+		</thead> 
+		<tbody> 
+			<tr> 
+			<td>Banco de Dados</th> 
+			<td>Mineração de dados</td> 
+			<td>Mestrado</td>
+			</tr> 
+			<tr> 
+			<td>Banco de Dados</th> 
+			<td>Analise de dados</td>
+			<td>Doutorado</td>
+			</tr> 
+			<tr> 
+			<td>Banco de Dados</th> 
+			<td>Mineração de dados</td> 
+			<td>Mestrado</td>
+			</tr> 
+			<tr> 
+			<td>Banco de Dados</th> 
+			<td>Analise de dados</td>
+			<td>Doutorado</td> 
+			</tr> 
+			<tr> 
+			<td>Banco de Dados</th> 
+			<td>Mineração de dados</td> 
+			<td>Mestrado</td>
+			</tr> 
+			<tr> 
+			<td>Banco de Dados</th> 
+			<td>Analise de dados</td>
+			<td>Doutorado</td>
+			</tr> 
+			<tr> 
+			<td>Banco de Dados</th> 
+			<td>Mineração de dados</td> 
+			<td>Mestrado</td>
+			</tr> 
+			<tr> 
+			<td>Banco de Dados</th> 
+			<td>Analise de dados</td>
+			<td>Doutorado</td> 
+			</tr> 
+			<tr> 
+			<td>Banco de Dados</th> 
+			<td>Mineração de dados</td> 
+			<td>Mestrado</td>
+			</tr> 
+			<tr> 
+			<td>Banco de Dados</th> 
+			<td>Analise de dados</td>
+			<td>Doutorado</td>
+			</tr> 
+			<tr> 
+			<td>Banco de Dados</th> 
+			<td>Mineração de dados</td> 
+			<td>Mestrado</td>
+			</tr> 
+			<tr> 
+			<td>Banco de Dados</th> 
+			<td>Analise de dados</td>
+			<td>Doutorado</td> 
+			</tr> 
+			<tr> 
+			<td>Banco de Dados</th> 
+			<td>Mineração de dados</td> 
+			<td>Mestrado</td>
+			</tr> 
+			<tr> 
+			<td>Banco de Dados</th> 
+			<td>Analise de dados</td>
+			<td>Doutorado</td>
+			</tr> 
+			<tr> 
+			<td>Banco de Dados</th> 
+			<td>Mineração de dados</td> 
+			<td>Mestrado</td>
+			</tr> 
+			<tr> 
+			<td>Banco de Dados</th> 
+			<td>Analise de dados</td>
+			<td>Doutorado</td> 
+			</tr> 
+		</tbody> 
+		</table>
+
+
+		<hr id="quatro">
+		<h2>Orientadores</h2>
+		<br>
+		<table class="table table-striped">
+		<thead> 
+			<tr> 
+			<th>Orientador</th> 
+			<th>Área</th> 
+			<th>Contato</th> 
+			</tr> 
+		</thead> 
+			<tbody> 
+			<tr> 
+			<th scope="row">Mark Silva</th> 
+			<td>Banco de Dados</td> 
+			<td>email@emialemail.com</td> 
+			</tr> 
+			<tr> 
+			<th scope="row">Jacob Matos</th> 
+			<td>Banco de Dados</td>
+			<td>email@emialemail.com</td> 
+			</tr> 
+			<tr> <th scope="row">Mark Silva</th> 
+			<td>Engenharia de Software</td> 
+			<td>email@emialemail.com</td> 
+			</tr> 
+			<tr> 
+			<th scope="row">Jacob Matos</th> 
+			<td>Engenharia de Software</td> 
+			<td>email@emialemail.com</td> 
+			</tr> 
+			<tr> 
+			<th scope="row">Mark Silva</th> 
+			<td>Banco de Dados</td>
+			<td>email@emialemail.com</td> 
+			</tr> 
+			<tr> <th scope="row">Jacob Matos</th> 
+			<td>Banco de Dados</td> 
+			<td>email@emialemail.com</td> 
+			</tr> 
+			<tr> 
+			<th scope="row">Mark Silva</th> 
+			<td>Banco de Dados</td> 
+			<td>email@emialemail.com</td> 
+			</tr> 
+			<tr> 
+			<th scope="row">Jacob Matos</th> 
+			<td>Banco de Dados</td>
+			<td>email@emialemail.com</td> 
+			</tr> 
+			<tr> <th scope="row">Mark Silva</th> 
+			<td>Engenharia de Software</td> 
+			<td>email@emialemail.com</td> 
+			</tr> 
+			<tr> 
+			<th scope="row">Jacob Matos</th> 
+			<td>Engenharia de Software</td> 
+			<td>email@emialemail.com</td> 
+			</tr> 
+			<tr> 
+			<th scope="row">Mark Silva</th> 
+			<td>Banco de Dados</td>
+			<td>email@emialemail.com</td> 
+			</tr> 
+			<tr> <th scope="row">Jacob Matos</th> 
+			<td>Banco de Dados</td> 
+			<td>email@emialemail.com</td> 
+			</tr>
+			<tr> 
+			<th scope="row">Mark Silva</th> 
+			<td>Banco de Dados</td> 
+			<td>email@emialemail.com</td> 
+			</tr> 
+			<tr> 
+			<th scope="row">Jacob Matos</th> 
+			<td>Banco de Dados</td>
+			<td>email@emialemail.com</td> 
+			</tr> 
+			<tr> <th scope="row">Mark Silva</th> 
+			<td>Engenharia de Software</td> 
+			<td>email@emialemail.com</td> 
+			</tr> 
+			<tr> 
+			<th scope="row">Jacob Matos</th> 
+			<td>Engenharia de Software</td> 
+			<td>email@emialemail.com</td> 
+			</tr> 
+			<tr> 
+			<th scope="row">Mark Silva</th> 
+			<td>Banco de Dados</td>
+			<td>email@emialemail.com</td> 
+			</tr> 
+			<tr> <th scope="row">Jacob Matos</th> 
+			<td>Banco de Dados</td> 
+			<td>email@emialemail.com</td> 
+			</tr> 
+		</tbody> 
+		</table>
+
+
+		<hr id="cinco">
+		<h2>Editais</h2>
+		<br>
+		<h3>Edital para Mestrado 2016</h3>
+		<br>
+
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+		<p><a href="http://uffs.edu.br" style="background-color:#ffffa0">Link para o Edital</a></p>
+
+		<br><br>
+		<h3>Edital para Doutorado 2016</h3>
+		<br>
+
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+		<p><a href="http://uffs.edu.br" style="background-color:#ffffa0">Link para o Edital</a></p>
+
+
+		<hr id="seis">
+		<h2>Bolsas</h2>
+		<br>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		<br><br>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		<br>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		<br><br>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		<br><br>
+		Regulamento para concorrer as bolsas:
+		<br><br>
+		<p><a href="http://uffs.edu.br" style="background-color:#ffffa0">- Mestrado </a></p> 
+		<p><a href="http://uffs.edu.br" style="background-color:#ffffa0">- Doutorado </a></p> 
+
+
+
+		<hr id="sete">
+		<h2>Horários</h2>
+		<br>
+		<table class="table table-striped">
+		<thead> 
+			<tr> 
+			<th>Disciplina</th> 
+			<th>Ministrante</th> 
+			<th>Horário</th> 
+			<th>Sala</th> 
+			</tr> 
+		</thead> 
+		<tbody> 
+			<tr> 
+			<th scope="row">MAC4722 - Linguagens, Autômatos e Computabilidade</th> 
+			<td>Mark</td> 
+			<td>Quarta-Feira 10:00</td> 
+			<td>201 A</td> 
+			</tr> 
+			<tr> 
+			<th scope="row">MAC5710 - Estrutura de Dados e sua Manipulação</th> 
+			<td>Jacob</td>
+			<td>Quarta-Feira 10:00</td> 
+			<td>201 A</td> </tr> 
+			<tr> <th scope="row">MAC5714 - Programação Orientada a Objetos</th> 
+			<td>Larry</td> 
+			<td>Quarta-Feira 10:00</td> 
+			<td>201 A</td> 
+			</tr> 
+			<tr> 
+			<th scope="row">MAC4722 - Linguagens, Autômatos e Computabilidade</th> 
+			<td>Mark</td> 
+			<td>Quarta-Feira 10:00</td> 
+			<td>201 A</td> 
+			</tr> 
+			<tr> 
+			<th scope="row">MAC5710 - Estrutura de Dados e sua Manipulação</th> 
+			<td>Jacob</td>
+			<td>Quarta-Feira 10:00</td> 
+			<td>201 A</td> </tr> 
+			<tr> <th scope="row">MAC5714 - Programação Orientada a Objetos</th> 
+			<td>Larry</td> 
+			<td>Quarta-Feira 10:00</td> 
+			<td>201 A</td> 
+			</tr> 
+			<tr> 
+			<th scope="row">MAC4722 - Linguagens, Autômatos e Computabilidade</th> 
+			<td>Mark</td> 
+			<td>Quarta-Feira 10:00</td> 
+			<td>201 A</td> 
+			</tr> 
+			<tr> 
+			<th scope="row">MAC5710 - Estrutura de Dados e sua Manipulação</th> 
+			<td>Jacob</td>
+			<td>Quarta-Feira 10:00</td> 
+			<td>201 A</td> </tr> 
+			<tr> <th scope="row">MAC5714 - Programação Orientada a Objetos</th> 
+			<td>Larry</td> 
+			<td>Quarta-Feira 10:00</td> 
+			<td>201 A</td> 
+			</tr>
+			<tr> 
+			<th scope="row">MAC4722 - Linguagens, Autômatos e Computabilidade</th> 
+			<td>Mark</td> 
+			<td>Quarta-Feira 10:00</td> 
+			<td>201 A</td> 
+			</tr> 
+			<tr> 
+			<th scope="row">MAC5710 - Estrutura de Dados e sua Manipulação</th> 
+			<td>Jacob</td>
+			<td>Quarta-Feira 10:00</td> 
+			<td>201 A</td> </tr> 
+			<tr> <th scope="row">MAC5714 - Programação Orientada a Objetos</th> 
+			<td>Larry</td> 
+			<td>Quarta-Feira 10:00</td> 
+			<td>201 A</td> 
+			</tr> 
+		</tbody> 
+		</table>
+
+	</div>
+
+</div>
+
+
+
+
+
