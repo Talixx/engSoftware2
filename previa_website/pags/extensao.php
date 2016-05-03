1a2ef609095ca683174c0a6b34489703bf43e686
Jackson Henrique Hochscheidt <jackson94h@gmail.com>
2016-05-03 15:37:39 -0300

    pagina com um esboço do conteudo que terá na aba extensão. Foi feito um menu de acesso rápido do lado direito da tela, para facilitar a navegação. Faltaria apenas melhorar a aparecia e a disposicao deste MENU na tela.

---
+++ previa_website/pags/extensao.php
@@ -0,0 +1,132 @@
+
+	<style type="text/css">
+
+	.article {
+	 	display: inline;
+	 	min-width: 60%;
+	 	max-width: 70%;
+	 	float: left;
+	}
+	.sidebar {
+		background-color: #228B22;
+		position:fixed;
+		display: inline;
+		max-height: 80%;
+		min-width: 20%;
+		max-width: 30%;
+		float: right;
+	}
+
+
+
+	</style>
+
+
+<div class="container">
+	<!--============== Features Layout ==============-->
+	<div class="article">
+		<h2 id="Projetos">Projetos</h2> <!-- <a class="button section-edit only-icon" ><i aria-hidden="true" class="icon-pencil"></i></a></h2>-->
+		<p> Hoje a UFFS conta com vários projetos de extensão. </p>
+		<p> ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+
+		<h2 id="ComoFuncionamProjetos">Como Funcionam?</h2>
+
+		<!--<h2 id="Atributos">Atributos<a class="button section-edit only-icon" href="/pt-BR/docs/Web/HTML/Element/aside$edit#Atributos"><i aria-hidden="true" class="icon-pencil"></i><span>Edit</span></a></h2>-->
+		<p>Os projetos de extensão da UFFS funcionam ... </p>
+		<p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</<p>
+		<h2 id="Parcerias">Parcerias</h2>
+
+		<p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+		<h2 id="ProjetosExistentes">Projetos Existentes</h2>
+		<p> Hoje no curso de Ciência da Computação existem os seguintes projetos de extensão:</p>
+			<h3 id="ClubeDeProgramacao">Clube de Programação</h3>
+			<p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+			<h3 id="Outros">Outros</h3>
+			<p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+		<h2 id=Estagios>Estágios</h2>
+		<p>Algum texto descrevendo as atividades de estágio no curso ou na instituição</p>
+		<p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<h3 id="VagasAbertas">Vagas Abertas</h3>
+			<p>Empresas que possuem vagas de estágio na área</p>
+			<p>Vagas de estágio na própria instituição</p>
+			<p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+			<h3 id="Editais">Editais</h3>
+			<ul>
+				<li>Vaga Estágio 1
+				</li>
+				<li>Vaga Estágio 2
+				</li>
+			</ul>
+
+	</div>
+
+
+	<div class="sidebar">
+		<ul class="listaNav">
+			<li><a href="#Projetos">Projetos</a>
+				<ul>
+					<li><a href="#ComoFuncionamProjetos" rel="internal">Como funcionam?</a>
+					</li>
+					<li><a href="#Parcerias" rel="internal">Parcerias</a>
+					</li>
+					<li><a href="#ProjetosExistentes" rel="internal">Projetos existentes</a>
+						<ul>
+							<li><a href="#ClubeDeProgramacao" rel="internal">Clube de Programação</a>
+							</li>
+							<li><a href="#Outros" rel="internal">Outros</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</li>
+			<li><a href="#Estagios">Estágios</a>
+				<ul>
+					<li><a href="#VagasAbertas" rel="internal">Vagas abertas</a>
+					</li>
+					<li><a href="#Editais" rel="internal">Editais</a>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</div>
